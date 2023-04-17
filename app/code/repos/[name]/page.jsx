82a34fcdd5294cc8fd244de83e7418bcb3a84b5b
Donice <technocrater.pdevelopers@gmail.com>
2023-04-17 10:34:12 +0100

    fetch API

---
+++ app/code/repos/[name]/page.jsx
@@ -0,0 +1,23 @@
+import { Suspense } from "react";
+import Link from "next/link";
+import Repo from "@/app/components/repo";
+import RepoDirs from "@/app/components/RepoDirs";
+
+// const RepoPage = ({ params: {name} }) => {
+const RepoPage = ({ params: { name } }) => {
+  return (
+    <div className="card">
+      <Link href="/code/repos" className="btn btn-back">
+        Back to repositories
+      </Link>
+      <Suspense fallback={<div>Loading repo...</div>}>
+        <Repo name={name} />
+      </Suspense>
+      <Suspense fallback={<div>Loading directories...</div>}>
+        <RepoDirs name={name} />
+      </Suspense>
+    </div>
+  );
+};
+
+export default RepoPage;
